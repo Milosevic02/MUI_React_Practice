7305cb9c2d8c0f6029821557056e4fe63060d4ec
Dragan <milosevicdragan002@gmail.com>
2024-04-08 14:29:52 +0200

    Add custom theme

--- lama_dev_tutorial/src/App.js
+++ lama_dev_tutorial/src/App.js
@@ -1,20 +1,29 @@
-import {Button} from "@mui/material"
+import {Button,styled} from "@mui/material"
 import AddHomeWorkIcon from '@mui/icons-material/AddHomeWork';
 
 function App() {
+  const BlueButton = styled(Button)(({theme})=>({
+    backgroundColor:"skyblue",
+    color :"#888",
+    margin:5,
+    "&:hover":{
+      backgroundColor:"lightblue"
+    },
+    "&:disabled":{
+      backgroundColor:"red",
+      color:"white"
+    }
+  }))
   return (
     <div>
-      <Button variant="text">Text</Button>
-      <Button startIcon={<AddHomeWorkIcon/>} variant="contained">Contained</Button>
-      <Button variant="contained" sx = {{
-          backgroundColor:"skyblue",
-          color :"#888",
-          margin:5,
-          "&:hover":{
-            backgroundColor:"lightblue"
-          }
+      <Button variant="text" color="otherColor">Text</Button>
+      <Button color="otherColor" startIcon={<AddHomeWorkIcon/>} variant="contained">Contained</Button>
+      <Button variant="contained"  sx = {{
+
       }}>My Unique Button</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button color="otherColor" variant="outlined">Outlined</Button>
+      <BlueButton>Prvooo</BlueButton>
+      <BlueButton>Drugoo</BlueButton>
     </div>
   );
 }
