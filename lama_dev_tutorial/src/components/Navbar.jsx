f8b1fa88fbdec1f4183d0ddb1c2912eff39a414f
Dragan <milosevicdragan002@gmail.com>
2024-04-08 20:48:03 +0200

    Finish Navbar

--- lama_dev_tutorial/src/components/Navbar.jsx
+++ lama_dev_tutorial/src/components/Navbar.jsx
@@ -1,8 +1,6 @@
-import styled from '@emotion/styled'
-import { Pets } from '@mui/icons-material';
-import { AppBar, Toolbar, Typography ,Box, InputBase} from '@mui/material'
-import {borderRadius} from "@mui/system"
-import React from 'react'
+import { Mail, Pets,Notifications } from '@mui/icons-material';
+import { AppBar, Toolbar, Typography ,Box, InputBase, Badge, Avatar,styled, Menu, MenuItem} from '@mui/material'
+import React, { useState } from 'react'
 
 const StyledToolbar = styled(Toolbar)({
     display:"flex",
@@ -10,18 +8,33 @@ const StyledToolbar = styled(Toolbar)({
 
 });
 
-const Search = styled("div")(({theme}) =>({
-    backgroundColor:"white",
-    padding:"0 10px",
-    width:"40%",
-    //borderRadius:theme.shape.borderRadius
-}))
+const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: theme.shape.borderRadius,
+    width: "40%",
+  }));
 
 const Icons = styled(Box)(({theme}) =>({
-    backgroundColor:"white"
-}))
+    display:"none",
+    gap:"20px",
+    alignItems:"center",
+    [theme.breakpoints.up("sm")]:{
+        display:"flex"
+    }
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  }));
 
 const Navbar = () => {
+    const [open,setOpen] = useState(false)
     return (
         <AppBar position="sticky">
             <StyledToolbar>
@@ -30,8 +43,39 @@ const Navbar = () => {
                 </Typography>
                 <Pets sx={{display:{xs:"block",sm:"none"}}}/>
                 <Search><InputBase placeholder='Search...'></InputBase></Search>
-                <Icons>Icons</Icons>
+                <Icons>
+                    <Badge badgeContent={4} color = "error">
+                        <Mail/>
+                    </Badge>
+                    <Badge badgeContent={2} color='error'>
+                        <Notifications/>
+                    </Badge>
+                    <Avatar onClick={e=>setOpen(true)} sx={{width:30,height:30}}src='https://mui.com/static/images/avatar/2.jpg' />
+
+                </Icons>
+                <UserBox onClick={e=>setOpen(true)}>
+                    <Avatar  sx={{width:30,height:30}}src='https://mui.com/static/images/avatar/2.jpg' />
+                    <Typography variant='span'>John</Typography>
+                </UserBox>
             </StyledToolbar>
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                open={open}
+                onClose={e=>setOpen(false)}
+                anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+                }}
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+                }}
+            >
+                <MenuItem >Profile</MenuItem>
+                <MenuItem >My account</MenuItem>
+                <MenuItem >Logout</MenuItem>
+            </Menu>
         </AppBar>
     )
 }
