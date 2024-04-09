021cbe6387c90d349b6ada54a6a573f8e63466a1
Dragan <milosevicdragan002@gmail.com>
2024-04-09 10:25:09 +0200

    Add Post component in Feed

--- lama_dev_tutorial/src/components/Feed.jsx
+++ lama_dev_tutorial/src/components/Feed.jsx
@@ -1,13 +1,20 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import Post from './Post'
 
 const Feed = () => {
     return (
         <Box
-        bgcolor="pink"
         flex={4}
         p={2}
-        >Feed</Box>
+        >
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+            <Post/>
+
+        </Box>
     )
 }
 
