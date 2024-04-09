6ca0bc063a99a3cf939040ecec12c5de1df90085
Dragan <milosevicdragan002@gmail.com>
2024-04-09 11:19:36 +0200

    Set dark theme func

--- lama_dev_tutorial/src/App.js
+++ lama_dev_tutorial/src/App.js
@@ -1,21 +1,31 @@
-import {Button,Container,Stack,styled,Box} from "@mui/material"
+import {createTheme,ThemeProvider,Stack,styled,Box,palette} from "@mui/material"
 import Sidebar from "./components/Sidebar";
 import Rightbar from "./components/Rightbar";
 import Feed from "./components/Feed";
 import Navbar from "./components/Navbar";
 import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode,setMode] = useState("light")
+  const darkTheme = createTheme({
+    palette:{
+      mode: mode
+    }
+  })
   return (
-    <Box>
-      <Navbar></Navbar>
-      <Stack direction="row" spacing={2} justifyContent={"space-between"}>
-        <Sidebar/>
-        <Feed/>
-        <Rightbar/>
-      </Stack>
-      <Add/>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar></Navbar>
+        <Stack direction="row" spacing={2} justifyContent={"space-between"}>
+          <Sidebar setMode={setMode} mode={mode}/>
+          <Feed/>
+          <Rightbar/>
+        </Stack>
+        <Add/>
+      </Box>
+    </ThemeProvider>
+
 
   );
 }
